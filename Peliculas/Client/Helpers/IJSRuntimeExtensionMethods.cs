6be967b77967f14b044cfc46f17b21ce11183070
Rafael Brenes <rbrenesr@gmail.com>
2023-09-23 07:21:26 -0600

    Seccion 3, Cap.38 Invocar metodos de Javascript desde c# [Desde una clase global static] {RB}

--- Peliculas/Client/Helpers/IJSRuntimeExtensionMethods.cs
+++ Peliculas/Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -1,6 +1,18 @@
-﻿namespace Peliculas.Client.Helpers
+﻿using Microsoft.JSInterop;
+using Peliculas.Shared.Entities;
+
+namespace Peliculas.Client.Helpers
 {
-    public class IJSRuntimeExtensionMethods
+    public static class IJSRuntimeExtensionMethods
     {
+        public static async ValueTask<bool> Confirm(this IJSRuntime iJSRuntime, string message) { 
+        
+            return await iJSRuntime.InvokeAsync<bool>("confirm", message);
+        }
+
+        public static void Clg(this IJSRuntime iJSRuntime, string message)
+        {
+            iJSRuntime.InvokeVoidAsync("console.log", message);
+        }
     }
 }

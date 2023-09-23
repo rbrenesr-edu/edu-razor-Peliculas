5f5f1b92848171bbd73c992fe9be47078eab79e0
Rafael Brenes <rbrenesr@gmail.com>
2023-09-23 06:24:16 -0600

    Seccion 3, cap.36 Clases Parciales - Separando código c# del HTML {RB}

---
+++ Peliculas/Client/Pages/Counter.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Peliculas.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject] ServicioSingleton servicioSingleton { get; set; } = null!;
+        [Inject] ServicioTransient servicioTransient { get; set; } = null!;
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            servicioSingleton.Valor = currentCount;
+            servicioTransient.Valor = currentCount;
+        }
+    }
+}

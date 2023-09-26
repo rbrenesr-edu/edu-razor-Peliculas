be3f9667e56e1869c9bd93f5cfd878abc6a59fcf
Rafael Brenes <rbrenesr@gmail.com>
2023-09-26 07:25:32 -0600

    Seccion 5, Cap.59 Formulario de actualización de géneros {RB}

--- Peliculas/Shared/Entities/Genero.cs
+++ Peliculas/Shared/Entities/Genero.cs
@@ -10,6 +10,8 @@ namespace Peliculas.Shared.Entities
     public class Genero
     {
         public int ID { get; set; }
+
+
         [Required(ErrorMessage = "El campo {0} es requerido.")]
         public string Nombre { get; set; }
     }

404055e00760427ff04d664c636e5a266ef87d91
Rafael Brenes <rbrenesr@gmail.com>
2023-09-28 07:49:45 -0600

    Seccion 5, Cap.63 Formulario Actores {RB}

---
+++ Peliculas/Shared/Entities/Actor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peliculas.Shared.Entities
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        
+        
+        [Required]        
+        public string? Nombre { get; set; }
+        public string? Biografia { get; set; }
+        public string? Foto { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+
+    }
+}

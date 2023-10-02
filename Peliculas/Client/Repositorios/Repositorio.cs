cd85004bbc5fd0aefb1f812465fdfdc605dbcd00
Rafael Brenes <rbrenesr@gmail.com>
2023-10-02 15:07:21 -0600

    Seccion 5, Cap.66 Formulario de Peliculas

--- Peliculas/Client/Repositorios/Repositorio.cs
+++ Peliculas/Client/Repositorios/Repositorio.cs
@@ -10,17 +10,17 @@ namespace Peliculas.Client.Repositorios
             {
                 new Pelicula{ 
                     Titulo = "Wakanda Forever", 
-                    FechaLanzamiento = new DateTime(2016,11,25),
+                    Lanzamiento = new DateTime(2016,11,25),
                     Poster="https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Black_Panther_Wakanda_Forever_poster.jpg/220px-Black_Panther_Wakanda_Forever_poster.jpg"
                 },
                 new Pelicula{ 
-                    Titulo = "Moana", 
-                    FechaLanzamiento = new DateTime(2020,10,25),
+                    Titulo = "Moana",
+                    Lanzamiento = new DateTime(2020,10,25),
                     Poster="https://upload.wikimedia.org/wikipedia/en/thumb/2/26/Moana_Teaser_Poster.jpg/220px-Moana_Teaser_Poster.jpg"
                 },
                 new Pelicula{ 
-                    Titulo = "Inception", 
-                    FechaLanzamiento = new DateTime(2023,9,15),
+                    Titulo = "Inception",
+                    Lanzamiento = new DateTime(2023,9,15),
                     Poster="https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg"
                 }
             };
--- Peliculas/Shared/Entities/Pelicula.cs
+++ Peliculas/Shared/Entities/Pelicula.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,14 @@ namespace Peliculas.Shared.Entities
 {
     public class Pelicula
     {
-        //public string? Titulo { get; set; }
-        //public required string Titulo { get; set; }
-        public string Titulo { get; set; } = null!;
-        public DateTime FechaLanzamiento { get; set; }
+        public int Id { get; set; }
+        [Required]public string Titulo { get; set; } = null!;
+        public string? Resumen { get; set; }
+        public bool EnCartelera { get; set; }
+        public string? Trailer { get; set; }
+        public DateTime? Lanzamiento { get; set; }
         public string? Poster { get; set; }
-        public string TituloCorto
+        public string? TituloCorto
         {
             get
             {

00e1a3a4b9f087a5da1ca9ec409d3c4a7b55331b
Rafael Brenes <rbrenesr@gmail.com>
2023-10-04 07:50:25 -0600

    Seccion 5, Cap- 68 Seleccion multiple - typeahead (autocomplete) Arrastrar elementos

--- Peliculas/Shared/Entities/Actor.cs
+++ Peliculas/Shared/Entities/Actor.cs
@@ -10,13 +10,28 @@ namespace Peliculas.Shared.Entities
     public class Actor
     {
         public int Id { get; set; }
-        
-        
-        [Required]        
+
+
+        [Required]
         public string? Nombre { get; set; }
         public string? Biografia { get; set; }
         public string? Foto { get; set; }
         public DateTime? FechaNacimiento { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Actor a2)
+            {
+                return Id == a2.Id;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
 
     }
 }

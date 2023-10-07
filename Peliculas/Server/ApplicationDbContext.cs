2707e72b680dd5335ce8efbb436de9d0f079accf
Rafael Brenes <rbrenesr@gmail.com>
2023-10-07 06:27:14 -0600

    Seccion 5, Cap.72 Instalando Entity Framework Core {RB}

---
+++ Peliculas/Server/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Peliculas.Server
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}

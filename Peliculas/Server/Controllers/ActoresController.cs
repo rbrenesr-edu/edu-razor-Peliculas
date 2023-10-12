f0fab7cef8c33f4ca372b963615a36d734730c57
Rafael Brenes <rbrenesr@gmail.com>
2023-10-12 15:40:32 -0600

    Seccion 6, Cap.77 Creando Actores {RB}

---
+++ Peliculas/Server/Controllers/ActoresController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Peliculas.Shared.Entities;
+
+namespace Peliculas.Server.Controllers
+{
+    [Route("api/actores")]
+    [ApiController]
+    public class ActoresController : ControllerBase        
+    {
+
+        private readonly ApplicationDbContext context;
+        public ActoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(Actor actor)
+        {
+            context.Add(actor);
+            await context.SaveChangesAsync();
+            return actor.Id;
+        }
+    }
+}

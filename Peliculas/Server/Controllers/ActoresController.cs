8fa1e806f82c830e5fd916a309dfc0cd1aa2779c
Rafael Brenes <rbrenesr@gmail.com>
2023-10-16 22:08:54 -0600

    Seccion 6, Cap.78 Preparando el proyecto para que guarde imagenes {RB}

--- Peliculas/Server/Controllers/ActoresController.cs
+++ Peliculas/Server/Controllers/ActoresController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Peliculas.Server.Helpers;
 using Peliculas.Shared.Entities;
 
 namespace Peliculas.Server.Controllers
@@ -10,15 +11,25 @@ namespace Peliculas.Server.Controllers
     {
 
         private readonly ApplicationDbContext context;
-        public ActoresController(ApplicationDbContext context)
+        private readonly IAlmacenadorArchivos almacenadorArchivos;
+        private readonly string contenedor = "personas";
+
+        public ActoresController(ApplicationDbContext context, IAlmacenadorArchivos almacenadorArchivos)
         {
             this.context = context;
+            this.almacenadorArchivos = almacenadorArchivos;
         }
 
 
         [HttpPost]
         public async Task<ActionResult<int>> Post(Actor actor)
         {
+            if (!string.IsNullOrWhiteSpace(actor.Foto))
+            {
+                var fotoActor = Convert.FromBase64String(actor.Foto);
+                actor.Foto = await almacenadorArchivos.GuardarArchivo(fotoActor, "jpg", contenedor);
+            }
+
             context.Add(actor);
             await context.SaveChangesAsync();
             return actor.Id;

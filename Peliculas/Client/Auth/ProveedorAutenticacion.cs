0d07f77e6b95bd1b145434bfe5d52881409e3348
Rafael Brenes <rbrenesr@gmail.com>
2023-11-03 08:50:58 -0600

    Seccion 7, Cap.99 Authentication State Provide de Prueba {RB}

---
+++ Peliculas/Client/Auth/ProveedorAutenticacion.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Peliculas.Client.Auth
+{
+    public class ProveedorAutenticacion : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(3000);
+            var anonimo = new ClaimsIdentity();
+            return await Task.FromResult(
+                new AuthenticationState(new ClaimsPrincipal(anonimo))
+                );
+        }
+    }
+}
--- Peliculas/Client/Program.cs
+++ Peliculas/Client/Program.cs
@@ -1,14 +1,18 @@
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Peliculas.Client;
+using Peliculas.Client.Auth;
 using Peliculas.Client.Repositorios;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 ConfigureServices(builder.Services);
 
 await builder.Build().RunAsync();
@@ -17,4 +21,6 @@ void ConfigureServices(IServiceCollection services) {
     services.AddSweetAlert2();
     services.AddScoped<IRepositorio, Repositorio>();
     //services.AddSingleton<IRepositorio, Repositorio>();
+    services.AddAuthorizationCore();
+    services.AddScoped<AuthenticationStateProvider, ProveedorAutenticacion>();
 }   

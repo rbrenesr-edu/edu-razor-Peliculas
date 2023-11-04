a12d704e075758c2cffcb9e16fea58c505fffcaf
Rafael Brenes <rbrenesr@gmail.com>
2023-11-04 13:49:24 -0600

    Seccion 7, Cap.100 AuthorizeView - Ocultabdo contenido visual según el estado de la autenticacion {RB}

--- Peliculas/Client/Auth/ProveedorAutenticacion.cs
+++ Peliculas/Client/Auth/ProveedorAutenticacion.cs
@@ -7,10 +7,22 @@ namespace Peliculas.Client.Auth
     {
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            await Task.Delay(3000);
+            await Task.Delay(1000);
             var anonimo = new ClaimsIdentity();
+
+            var user = new ClaimsIdentity(
+                
+                new List<Claim> 
+                {
+                    new Claim("id","304140227"),    
+                    new Claim(ClaimTypes.Name,"Rafael Brenes"),
+                    new Claim(ClaimTypes.Role, "admins")
+
+                },
+                authenticationType: "test");
+
             return await Task.FromResult(
-                new AuthenticationState(new ClaimsPrincipal(anonimo))
+                new AuthenticationState(new ClaimsPrincipal(user))
                 );
         }
     }

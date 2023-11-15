8d92dcd7a1e4d9d73c8857d9bfa40638bd9bd2b7
Rafael Brenes <rbrenesr@gmail.com>
2023-11-15 13:08:43 -0600

    Seccion 7 Cap.111 Mostrando el Listado de Usuarios {RB}

---
+++ Peliculas/Server/Controllers/UsuariosController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Peliculas.Server.Helpers;
+using Peliculas.Shared.DTOs;
+
+namespace Peliculas.Server.Controllers
+{
+    [ApiController]
+    [Route("api/usuarios")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public UsuariosController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UsuarioDTO>>> Get([FromQuery] PaginacionDTO paginacionDTO) { 
+
+            var queryable = context.Users.AsQueryable();
+            await HttpContext.InsertarParametrosPaginacionEnRespuesta(queryable, paginacionDTO.CantidadRegistros);
+            return await queryable.Paginar(paginacionDTO)
+                .Select(x =>
+                new UsuarioDTO { Id = x.Id, Email = x.Email }
+                ).ToListAsync();
+
+
+
+        }
+    }
+}

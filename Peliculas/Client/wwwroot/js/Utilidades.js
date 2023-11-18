431e033706762fe1afe76cbf339aea8c0e9cd957
Rafael Brenes <rbrenesr@gmail.com>
2023-11-18 09:23:47 -0600

    Seccion 7, Cap.114 Deslogueo automnático -FIN DE LA SESSION {RB}

--- Peliculas/Client/wwwroot/js/Utilidades.js
+++ Peliculas/Client/wwwroot/js/Utilidades.js
@@ -5,7 +5,21 @@ function testNetStatic() {
         })
 }
 
-
 function testNetInstancia(dotNetHelper) {
     dotNetHelper.invokeMethodAsync("IncrementCount");        
+}
+
+function timerInactivo(dotNetHelper){
+    var timer;
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+
+    function resetTimer(){
+        clearTimeout(timer);
+        timer = setTimeout(logout, 60*1000) //3 segundos
+    }
+
+    function logout() {
+        dotNetHelper.invokeMethodAsync("Logout");
+    }
 }
